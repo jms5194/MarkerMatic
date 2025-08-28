db120d8f63c33d64eb3eafbe3969cb129df63dde
jms5194 <justinstasiw@gmail.com>
2025-08-27 20:17:17 -0400

    troubleshooting py2app code signing

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ OPTIONS = {
         "resources/MarkerMatic-Bridge.bwextension",
     ],
     "includes": ["mido.backends.portmidi"],
+    "excludes": ["pyinstaller", "pyinstaller-hooks-contrib"],
     "dylib_excludes": [
         "/Library/Frameworks/Python.framework/Versions/3.13/Frameworks/Tcl.framework",
         "/Library/Frameworks/Python.framework/Versions/3.13/Frameworks/Tk.framework",
