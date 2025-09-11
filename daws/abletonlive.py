c423e1d0b9edf5e594c02a6b1648fa011a62ca64
jms5194 <justinstasiw@gmail.com>
2025-09-10 21:02:39 -0400

    starting ideas for ableton live

--- daws/abletonlive.py
+++ daws/abletonlive.py
@@ -21,11 +21,11 @@ class Ableton(Daw):
 
     def __init__(self):
         super().__init__()
-        self.reaper_send_lock = threading.Lock()
+        self.ableton_send_lock = threading.Lock()
         self.name_to_match = ""
         self.is_playing = False
         self.is_recording = False
-        self.reaper_osc_server = None
+        self.ableton_osc_server = None
         pub.subscribe(
             self._place_marker_with_name, PyPubSubTopics.PLACE_MARKER_WITH_NAME
         )
@@ -37,12 +37,9 @@ class Ableton(Daw):
     def start_managed_threads(
         self, start_managed_thread: Callable[[str, Any], None]
     ) -> None:
-        logger.info("Starting Reaper Connection threads")
+        logger.info("Starting Ableton Connection threads")
         self._shutdown_server_event.clear()
-        start_managed_thread(
-            "validate_reaper_prefs_thread", self._validate_reaper_prefs
-        )
-        start_managed_thread("daw_connection_thread", self._build_reaper_osc_servers)
+        start_managed_thread("daw_connection_thread", self._build_ableton_osc_servers)
         start_managed_thread("daw_connection_monitor", self._daw_connection_monitor)
 
     def _daw_connection_monitor(self):
@@ -62,72 +59,31 @@ class Ableton(Daw):
                     )
                     self._connection_timeout_counter = 0
 
-    def _validate_reaper_prefs(self):
-        # If the Reaper .ini file does not contain an entry for Digico-Reaper Link, add one.
-        from app_settings import settings
+    def _build_ableton_osc_servers(self):
+        # Connect to Ableton via OSC
 
-        while not self._shutdown_server_event.is_set():
-            try:
-                if not self._check_reaper_prefs(
-                    settings.reaper_receive_port, settings.reaper_port
-                ):
-                    self._add_reaper_prefs(
-                        settings.reaper_receive_port, settings.reaper_port
-                    )
-                    pub.sendMessage(
-                        PyPubSubTopics.REQUEST_DAW_RESTART, daw_name="Reaper"
-                    )
-                return True
-            except RuntimeError:
-                # If reaper is not running, wait and try again
-                logger.error("Reaper not running. Will retry in 1 seconds.")
-                time.sleep(1)
-        return None
-
-    @staticmethod
-    def _check_reaper_prefs(rpr_rcv, rpr_send):
-        if configure_reaper.osc_interface_exists(
-            configure_reaper.get_resource_path(True), rpr_rcv, rpr_send
-        ):
-            logger.info("Reaper OSC interface config already exists")
-            return True
-        else:
-            logger.info("Reaper OSC interface config does not exist")
-            return False
-
-    @staticmethod
-    def _add_reaper_prefs(rpr_rcv, rpr_send):
-        configure_reaper.add_OSC_interface(
-            configure_reaper.get_resource_path(True), rpr_rcv, rpr_send
+        logger.info("Starting Ableton OSC server")
+        self.ableton_client = udp_client.SimpleUDPClient(
+            constants.IP_LOOPBACK, constants.PORT_ABLETON_OSC_SEND
         )
-        logger.info("Added OSC interface to Reaper preferences")
-
-    def _build_reaper_osc_servers(self):
-        # Connect to Reaper via OSC
-        from app_settings import settings
-
-        logger.info("Starting Reaper OSC server")
-        self.reaper_client = udp_client.SimpleUDPClient(
-            constants.IP_LOOPBACK, settings.reaper_port
-        )
-        self.reaper_dispatcher = dispatcher.Dispatcher()
-        self._receive_reaper_OSC()
+        self.ableton_dispatcher = dispatcher.Dispatcher()
+        self._receive_ableton_OSC()
         try:
-            self.reaper_osc_server = osc_server.ThreadingOSCUDPServer(
-                (constants.IP_LOOPBACK, settings.reaper_receive_port),
-                self.reaper_dispatcher,
+            self.ableton_osc_server = osc_server.ThreadingOSCUDPServer(
+                (constants.IP_LOOPBACK, constants.PORT_ABLETON_OSC_RECEIVE),
+                self.ableton_dispatcher,
             )
-            logger.info("Reaper OSC server started")
-            self.reaper_osc_server.serve_forever()
+            logger.info("Ableton OSC server started")
+            self.ableton_osc_server.serve_forever()
         except Exception as e:
-            logger.error(f"Reaper OSC server startup error: {e}")
+            logger.error(f"Ableton OSC server startup error: {e}")
 
-    def _receive_reaper_OSC(self):
+    def _receive_ableton_OSC(self):
         # Receives and distributes OSC from Reaper, based on matching OSC values
-        self.reaper_dispatcher.map("/marker/*/name", self._marker_matcher)
-        self.reaper_dispatcher.map("/play", self._current_transport_state)
-        self.reaper_dispatcher.map("/record", self._current_transport_state)
-        self.reaper_dispatcher.set_default_handler(self._message_received)
+        self.ableton_dispatcher.map("/marker/*/name", self._marker_matcher)
+        self.ableton_dispatcher.map("/play", self._current_transport_state)
+        self.ableton_dispatcher.map("/record", self._current_transport_state)
+        self.ableton_dispatcher.set_default_handler(self._message_received)
 
     def _message_received(self, *_) -> None:
         if not self._connected.is_set():
@@ -154,7 +110,7 @@ class Ableton(Daw):
 
     def _current_transport_state(self, osc_address, val):
         self._message_received()
-        # Watches what the Reaper playhead is doing.
+        # Watches what the Ableton playhead is doing.
         playing = None
         recording = None
         if osc_address == "/play":
@@ -169,30 +125,30 @@ class Ableton(Daw):
                 recording = True
         if playing is True:
             self.is_playing = True
-            logger.info("Reaper is playing")
+            logger.info("Ableton is playing")
         elif playing is False:
             self.is_playing = False
-            logger.info("Reaper is not playing")
+            logger.info("Ableton is not playing")
         if recording is True:
             self.is_recording = True
-            logger.info("Reaper is recording")
+            logger.info("Ableton is recording")
         elif recording is False:
             self.is_recording = False
-            logger.info("Reaper is not recording")
+            logger.info("Ableton is not recording")
 
     def _refresh_control_surfaces(self) -> None:
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/action", 41743)
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/action", 41743)
 
     def _goto_marker_by_id(self, marker_id):
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/marker", int(marker_id))
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/marker", int(marker_id))
 
     def _place_marker_with_name(self, marker_name: str):
         logger.info(f"Placed marker for cue: {marker_name}")
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/action", 40157)
-            self.reaper_client.send_message("/lastmarker/name", marker_name)
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/action", 40157)
+            self.ableton_client.send_message("/lastmarker/name", marker_name)
 
     def get_marker_id_by_name(self, name: str):
         # Asks for current marker information based upon number of markers.
@@ -204,31 +160,31 @@ class Ableton(Daw):
                 self.name_to_match = self.name_to_match.split(" ")
                 self.name_to_match = self.name_to_match[1:]
                 self.name_to_match = " ".join(self.name_to_match)
-            with self.reaper_send_lock:
-                self.reaper_client.send_message("/device/marker/count", 0)
+            with self.ableton_send_lock:
+                self.ableton_client.send_message("/device/marker/count", 0)
                 # Is there a better way to handle this in OSC only? Max of 512 markers.
-                self.reaper_client.send_message("/device/marker/count", 512)
+                self.ableton_client.send_message("/device/marker/count", 512)
 
     def _incoming_transport_action(self, transport_action: TransportAction):
         try:
             if transport_action is TransportAction.PLAY:
-                self._reaper_play()
+                self._ableton_play()
             elif transport_action is TransportAction.STOP:
-                self._reaper_stop()
+                self._ableton_stop()
             elif transport_action is TransportAction.RECORD:
-                self._reaper_rec()
+                self._ableton_rec()
         except Exception as e:
             logger.error(f"Error processing transport macros: {e}")
 
-    def _reaper_play(self):
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/action", 1007)
+    def _ableton_play(self):
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/live/song/start_playing", None)
 
-    def _reaper_stop(self):
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/action", 1016)
+    def _ableton_stop(self):
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/live/song/stop_playing", None)
 
-    def _reaper_rec(self):
+    def _ableton_rec(self):
         # Sends action to skip to end of project and then record, to prevent overwrites
         from app_settings import settings
 
@@ -236,9 +192,8 @@ class Ableton(Daw):
         pub.sendMessage(
             PyPubSubTopics.CHANGE_PLAYBACK_STATE, selected_mode=PlaybackState.RECORDING
         )
-        with self.reaper_send_lock:
-            self.reaper_client.send_message("/action", 40043)
-            self.reaper_client.send_message("/action", 1013)
+        with self.ableton_send_lock:
+            self.ableton_client.send_message("/live/song/trigger_session_record", None)
 
     def _handle_cue_load(self, cue: str) -> None:
         from app_settings import settings
@@ -256,9 +211,9 @@ class Ableton(Daw):
 
     def _shutdown_servers(self):
         try:
-            if self.reaper_osc_server:
-                self.reaper_osc_server.shutdown()
-                self.reaper_osc_server.server_close()
-            logger.info("Reaper OSC Server shutdown completed")
+            if self.ableton_osc_server:
+                self.ableton_osc_server.shutdown()
+                self.ableton_osc_server.server_close()
+            logger.info("Ableton OSC Server shutdown completed")
         except Exception as e:
-            logger.error(f"Error shutting down Reaper server: {e}")
+            logger.error(f"Error shutting down Ableton server: {e}")
